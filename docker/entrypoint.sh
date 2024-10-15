5dad35164433c0106c505ada2cdaf19538479f0a
Evgeniy Kolesnikov <eugene@Evgeniys-MacBook-Air.local>
2024-10-15 20:28:19 +0300

    rework first initial db

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,5 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-# python manage.py loaddata backups/first_initial.json
 # gunicorn vcrm.wsgi --bind 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
